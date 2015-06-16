272d14fd93437b3a5c1b599708bef4f0ae2eca23
Somnuk Wongkhan <somnuk.wk@gmail.com>
2015-06-16 11:07:08 +0700

    json provider graph is ok if reset interactive session

--- TourismWeb.Automate/JsonProviderSpec.fsx
+++ TourismWeb.Automate/JsonProviderSpec.fsx
@@ -1,33 +1,15 @@
-﻿
-
-#if INTERACTIVE
-#I @"..\packages\FSharp.Data.2.2.2\lib\net40"
-#I @"..\packages\canopy.0.9.22\lib"
-#I @"..\packages\Selenium.WebDriver.2.45.0\lib\net40"
-#r "canopy.dll"
-#r "WebDriver.dll"
+﻿#I @"..\packages\FSharp.Data.2.2.2\lib\net40"
 #r "FSharp.Data.dll"
-#r "Newtonsoft.Json.dll"
-#endif
 
-#if INTERACTIVE
-let path = __SOURCE_DIRECTORY__ 
-#else
-let path = "./"
-#endif
-
-open System
-open OpenQA.Selenium
-open canopy.configuration
-open canopy.runner
-open canopy.reporters
-open canopy.types
-open canopy
 open System.IO
-open System.Linq
-open Newtonsoft.Json
-open FSharp.Data
 
+do 
+    let path = __SOURCE_DIRECTORY__ 
+    Directory.SetCurrentDirectory(path)
 
-type Property = FSharp.Data.JsonProvider<"Property.json">
+type Property = FSharp.Data.JsonProvider< "Info.json" >
 
+let info = File.ReadAllText("Info.json") |> Property.Parse
+
+let n1 = info.A.Title
+let n2 = info.B.Policeliasion.FirstName
